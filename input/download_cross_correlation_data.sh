0b2e02772be8dd820ba614f8c7a15e18be0edb87
Thomas Callister <tcallister@Thomass-MacBook-Air.local>
2023-05-05 10:23:24 -0500

    Adding script to download cross-correlation data:

---
+++ input/download_cross_correlation_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -o H1L1_O1.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O1.dat
+curl -o H1L1_O2.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O2.dat
+curl -o H1L1_O3.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O3_HL.dat
+curl -o H1V1_O3.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O3_HV.dat
+curl -o L1V1_O3.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O3_LV.dat
