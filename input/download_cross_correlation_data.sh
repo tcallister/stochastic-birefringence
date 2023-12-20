2e7524944f6eba7c8cf6a6e4fd72b6c3b011ca52
Thomas Callister <tcallister@Thomass-MacBook-Air.local>
2023-12-20 11:12:04 -0600

    Updating cross-corr download script

--- input/download_cross_correlation_data.sh
+++ input/download_cross_correlation_data.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Cross-correlation data for each baseline and observing run
 curl -o H1L1_O1.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O1.dat
 curl -o H1L1_O2.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O2.dat
 curl -o H1L1_O3.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O3_HL.dat
 curl -o H1V1_O3.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O3_HV.dat
 curl -o L1V1_O3.dat https://dcc.ligo.org/public/0169/G2001287/005/C_O3_LV.dat
+
+# Anticipated design-sensitivity PI curve computed by the LVK
+curl -o Design_HLV_flow_10.txt https://dcc.ligo.org/public/0169/G2001287/005/PICurve_HLV_Design.dat
